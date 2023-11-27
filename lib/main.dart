96168425eaf235ae86cc22dd126f1eb15758b581
Abhay Rajeev <abhayrajeevkk2001@gmail.com>
2023-11-27 19:34:56 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:calculator_app/view/Calculator_Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class Calculator_app extends StatelessWidget {
+  const Calculator_app({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Homepage(),
+    );
+  }
+}
